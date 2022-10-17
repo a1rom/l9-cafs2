83eba5fe2fcacebb89479a9e44aa71cfd38736bc
alrom12 <dev@alrom.de>
2022-10-17 20:16:17 +0300

    wip layouts

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/products">Products</a>
+            </li>
+            <li>
+                <a href="/products/create">Create Product</a>
+            </li>
+        </ul>
+    </nav>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>Footer</p>
+    </footer>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section('title', 'Products')
+{{-- @endsection --}}
+
+@section('content')
+    <h1>Products</h1>
+    <ul>
+        @foreach ($products as $product)
+        <table>
+            <tr>
+                <td>
+                    <li>
+                        <a href="/products/{{ $product->id }}">
+                            {{ $product->name }}
+                        </a>
+                    </li>
+                </td>
+                <td>
+                    <form action="/products/{{ $product->id }}" method="GET">
+                        @csrf
+                        @method('GET')
+                        <button type="submit">View</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="/products/{{ $product->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            {{-- <li>
+                <a href="/products/{{ $product->id }}">
+                    {{ $product->name }}
+                </a>
+            </li> --}}
+        @endforeach
+    </ul>
+@endsection
